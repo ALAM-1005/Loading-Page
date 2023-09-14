f071b55db50bd9c896e53e22fc6bfe5433ac4108
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2023-09-15 00:29:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
